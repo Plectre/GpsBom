c98ef38c00f20c4efd9510401a2b4b45fe14ccf3
Plectre <alvarezido@gmail.com>
2017-03-25 19:18:40 +0100

    Merge branch 'master' of E:\AndroidStudioProjects\GpsBom with conflicts.

--- app/src/main/java/gpsbom/plectre/com/gpsbom/SaveBox.java
+++ app/src/main/java/gpsbom/plectre/com/gpsbom/SaveBox.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.Fragment;
+import android.content.ComponentName;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v4.app.FragmentActivity;
@@ -14,7 +16,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 
 /**
@@ -28,16 +29,12 @@ import android.widget.Toast;
     public Dialog d;
     public Button save,cancel;
     public EditText edText;
-    public Boolean receptData = false;
-    private String fichierName;
-
-
 
     public SaveBox(Activity a) {
         super(a);
         this.c = a;
-    }
 
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,42 +47,25 @@ import android.widget.Toast;
         cancel.setOnClickListener(this);
 
     }
-
-    // gestion des boutons cliqués
     @Override
     public void onClick(View v) {
-        switch(v.getId()) {
+        switch (v.getId()) {
             case R.id.btn_saveFichierName:
-                // Test si le champs est rempli
-                if (edText.getText().toString().equals("")) {
-                    dialogIsEmpty();
-                } else {
-                    Log.e("edText :", "!= null");
-                    edTexSaveOK(v);
-                }
-                break;
-            case R.id.btn_cancel:
-                dismiss();
-                break;
-            default:
-                Log.e("Default", "Default");
-                break;
-        }
-    }
-
-    public void dialogIsEmpty() {
-        Toast.makeText(getContext(), "Remplir tous les champs", Toast.LENGTH_LONG).show();
-    }
-
-    // Methode céation fichier
-    public void edTexSaveOK(View v) {
-                fichierName = String.valueOf(edText.getText());
+                String fichierName = String.valueOf(edText.getText());
                 Log.e("SAVE", fichierName);
                 // Vérification des fichiers d'enregistrement
                 SaveFiles saveDirectory = new SaveFiles();
                 saveDirectory.testCarteSd(fichierName);
                 dismiss();
-                receptData = true;
-        }
 
+                break;
+            case R.id.btn_cancel:
+                Log.e("cancel", "cancel");
+                dismiss();
+                break;
+            default:
+                Log.e("Default","default");
+                break;
+        }
     }
+}
